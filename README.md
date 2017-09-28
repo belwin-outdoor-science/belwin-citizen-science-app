cc9b5384bf1c780acdd28c4f2c875e6a96db5887
Brendt Bly <brendt.bly@outlook.com>
2017-09-28 14:14:29 -0500

    initial file structure

---
+++ README.md
@@ -0,0 +1,1 @@
+Belwin science project
---
+++ data.sql
@@ -0,0 +1,356 @@
+CREATE TABLE bur_oak (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+breaking_leaf_buds VARCHAR(80),
+
+leaves VARCHAR(80),
+
+increasing_leaf_size VARCHAR(80),
+
+colored_leaves VARCHAR(80),
+
+falling_leaves VARCHAR(80),
+
+flowers_or_flower_buds VARCHAR(80),
+
+open_flowers VARCHAR(80),
+
+pollen_release VARCHAR(80),
+
+fruits VARCHAR(80),
+
+ripe_fruits VARCHAR(80),
+
+recent_fruit_or_seed_drop VARCHAR (80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE common_buckthorn (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+breaking_leaf_buds VARCHAR(80),
+
+leaves VARCHAR(80),
+
+increasing_leaf_size VARCHAR(80),
+
+colored_leaves VARCHAR(80),
+
+falling_leaves VARCHAR(80),
+
+flowers_or_flower_buds VARCHAR(80),
+
+open_flowers VARCHAR(80),
+
+pollen_release VARCHAR(80),
+
+fruits VARCHAR(80),
+
+ripe_fruits VARCHAR(80),
+
+recent_fruit_or_seed_drop VARCHAR (80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE common_milkweed (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+initial_growth VARCHAR(80),
+
+leaves VARCHAR(80),
+
+flowers_or_flower_buds VARCHAR(80),
+
+open_flowers VARCHAR(80),
+
+fruits VARCHAR(80),
+
+ripe_fruits VARCHAR(80),
+
+recent_fruit_or_seed_drop VARCHAR (80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE eastern_bluebird (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+active_individuals VARCHAR(80),
+
+feeding VARCHAR(80),
+
+fruit_seed_consumption VARCHAR(80),
+
+insect_consumption VARCHAR(80),
+
+calls_or_song VARCHAR(80),
+
+singing_individuals VARCHAR(80),
+
+territorial_individuals VARCHAR(80),
+
+courtship VARCHAR(80),
+
+mating VARCHAR(80),
+
+nest_building VARCHAR(80),
+
+occupied_nest VARCHAR(80),
+
+nestlings VARCHAR(80),
+
+fledged_young VARCHAR(80),
+
+dead_individuals VARCHAR(80),
+
+dead_nestlings_or_fledlings VARCHAR(80),
+
+individuals_at_feeding_station VARCHAR(80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE ground_squirrel (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+active_individuals VARCHAR(80),
+
+feeding VARCHAR(80),
+
+young_individuals VARCHAR(80),
+
+dead_individuals VARCHAR(80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE dark_eyed_junco (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+active_individuals VARCHAR(80),
+
+feeding VARCHAR(80),
+
+fruit_seed_consumption VARCHAR(80),
+
+insect_consumption VARCHAR(80),
+
+calls_or_song VARCHAR(80),
+
+singing_individuals VARCHAR(80),
+
+territorial_individuals VARCHAR(80),
+
+courtship VARCHAR(80),
+
+mating VARCHAR(80),
+
+nest_building VARCHAR(80),
+
+occupied_nest VARCHAR(80),
+
+nestlings VARCHAR(80),
+
+fledged_young VARCHAR(80),
+
+dead_individuals VARCHAR(80),
+
+dead_nestlings_or_fledlings VARCHAR(80),
+
+individuals_at_feeding_station VARCHAR(80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE paper_birch (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+breaking_leaf_buds VARCHAR(80),
+
+leaves VARCHAR(80),
+
+increasing_leaf_size VARCHAR(80),
+
+colored_leaves VARCHAR(80),
+
+falling_leaves VARCHAR(80),
+
+flowers_or_flower_buds VARCHAR(80),
+
+open_flowers VARCHAR(80),
+
+pollen_release VARCHAR(80),
+
+fruits VARCHAR(80),
+
+ripe_fruits VARCHAR(80),
+
+recent_fruit_or_seed_drop VARCHAR (80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE quaking_aspen (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+breaking_leaf_buds VARCHAR(80),
+
+leaves VARCHAR(80),
+
+increasing_leaf_size VARCHAR(80),
+
+colored_leaves VARCHAR(80),
+
+falling_leaves VARCHAR(80),
+
+flowers_or_flower_buds VARCHAR(80),
+
+open_flowers VARCHAR(80),
+
+pollen_release VARCHAR(80),
+
+fruits VARCHAR(80),
+
+ripe_fruits VARCHAR(80),
+
+recent_fruit_or_seed_drop VARCHAR (80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE northern_red_oak (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+breaking_leaf_buds VARCHAR(80),
+
+leaves VARCHAR(80),
+
+increasing_leaf_size VARCHAR(80),
+
+colored_leaves VARCHAR(80),
+
+falling_leaves VARCHAR(80),
+
+flowers_or_flower_buds VARCHAR(80),
+
+open_flowers VARCHAR(80),
+
+pollen_release VARCHAR(80),
+
+fruits VARCHAR(80),
+
+ripe_fruits VARCHAR(80),
+
+recent_fruit_or_seed_drop VARCHAR (80),
+
+notes VARCHAR (250)
+
+);
+
+
+CREATE TABLE ruby_throated_hummingbird (
+
+id SERIAL PRIMARY KEY,
+
+recorded TIMESTAMP DEFAULT current_date,
+
+class VARCHAR(80),
+
+active_individuals VARCHAR(80),
+
+feeding VARCHAR(80),
+
+insect_consumption VARCHAR(80),
+
+flower_visitation VARCHAR(80),
+
+calls_or_song VARCHAR(80),
+
+singing_individuals VARCHAR(80),
+
+territorial_individuals VARCHAR(80),
+
+courtship VARCHAR(80),
+
+mating VARCHAR(80),
+
+nest_bulding VARCHAR(80),
+
+occupied_nest VARCHAR(80),
+
+nestlings VARCHAR(80),
+
+fledged_young VARCHAR(80),
+
+dead_individuals VARCHAR(80),
+
+dead_nestlings_or_fledlings VARCHAR(80),
+
+individuals_at_feeding_station VARCHAR(80),
+
+notes VARCHAR (250)
+
+);
