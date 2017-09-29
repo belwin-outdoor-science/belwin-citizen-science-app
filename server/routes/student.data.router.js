899be5b98914dc11e4abc385703f7be77b642f4a
Charles Renk <renkx008@umn.edu>
2017-09-29 10:00:44 -0500

    more edits with navbar

--- server/routes/student.data.router.js
+++ server/routes/student.data.router.js
@@ -190,26 +190,26 @@ router.post('/northern_red_oak', function (req, res) {
     });
 });
 
-router.post('/ruby_throated_hummingbird', function (req, res) {
-    var newHummingbird = req.body;
-    console.log('ruby_throated_hummingbird router post called ');
-    pool.connect(function (err, client, done) {
-        if (err) {
-            console.log('Error connecting to database', err);
-            res.sendStatus(500);
-        } else {
-            client.query('INSERT INTO ruby_throated_hummingbird (class, active_individuals, feeding, insect_consumption, flower_visitation, calls_or_song, singing_individuals, territorial_individuals, courtship, mating, nest_bulding, occupied_nest, nestlings, fledged_young, dead_individuals, dead_nestlings_or_fledlings, individuals_at_feeding_station, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18);',
-                [class, active_individuals, feeding, insect_consumption, flower_visitation, calls_or_song, singing_individuals, territorial_individuals, courtship, mating, nest_bulding, occupied_nest, nestlings, fledged_young, dead_individuals, dead_nestlings_or_fledlings, individuals_at_feeding_station, notes]);
-            done();
-            if (err) {
-                console.log('Error making ruby_throated_hummingbird post query: ', err);
-                res.sendStatus(500);
-            } else {
-                res.sendStatus(201);
-            }
-        }
-    });
-});
+// router.post('/ruby_throated_hummingbird', function (req, res) {
+//     var newHummingbird = req.body;
+//     console.log('ruby_throated_hummingbird router post called ');
+//     pool.connect(function (err, client, done) {
+//         if (err) {
+//             console.log('Error connecting to database', err);
+//             res.sendStatus(500);
+//         } else {
+//             client.query('INSERT INTO ruby_throated_hummingbird (class, active_individuals, feeding, insect_consumption, flower_visitation, calls_or_song, singing_individuals, territorial_individuals, courtship, mating, nest_bulding, occupied_nest, nestlings, fledged_young, dead_individuals, dead_nestlings_or_fledlings, individuals_at_feeding_station, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18);',
+//                 [class, active_individuals, feeding, insect_consumption, flower_visitation, calls_or_song, singing_individuals, territorial_individuals, courtship, mating, nest_bulding, occupied_nest, nestlings, fledged_young, dead_individuals, dead_nestlings_or_fledlings, individuals_at_feeding_station, notes]);
+//             done();
+//             if (err) {
+//                 console.log('Error making ruby_throated_hummingbird post query: ', err);
+//                 res.sendStatus(500);
+//             } else {
+//                 res.sendStatus(201);
+//             }
+//         }
+//     });
+// });
 
 
 module.exports = router;
