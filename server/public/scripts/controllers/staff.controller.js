766caa94b0cde946e52123dafc3a8fecb59b7537
rkiang <ryanlkiang@gmail.com>
2017-10-02 13:12:59 -0500

    added all get routes

--- server/public/scripts/controllers/staff.controller.js
+++ server/public/scripts/controllers/staff.controller.js
@@ -1,12 +1,20 @@
-myApp.controller('StaffController', ['StaffService', 'DataService', '$routeParams', function (StaffService, DataService, $routeParams) {
+myApp.controller('StaffController', ['StaffService', 'DataService', function (StaffService, DataService) {
     console.log('Staff Controller is sourced in');
     var vm = this;
     vm.user = StaffService;
     vm.data = DataService;
     StaffService.getUserInfo();
-    // DataService.getData();
-    // DataService.getBuckthorn();
-    console.log('data:', vm.data);
-    
+    vm.data.getBur();
+    vm.data.getBuckthorn();
+    vm.data.getMilkweed();
+    vm.data.getDark();
+    vm.data.getEastern();
+    vm.data.getGround();
+    vm.data.getNorthern();
+    vm.data.getPaper();
+    vm.data.getQuaking();
+    vm.data.getRuby();
+
+    console.log('vm.data:', vm.data);
     
 }])
