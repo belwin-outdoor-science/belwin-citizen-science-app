5da318aa279740fc01a2dd92c286dd6263fb8572
Ami Schroeder <arschroeder22@gmail.com>
2017-10-10 08:54:05 -0500

    user list done. can delete with confirmation. some users are not deleteable

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -9,25 +9,39 @@ myApp.controller('UserController', ['UserService', 'StaffService', '$routeParams
 
   StaffService.getUsers();
 
-  vm.deleteUser = function (userId) {
-    StaffService.deleteUser(userId);
+  vm.deleteUser = function (user) {
+    if (user.deleteable === false) {
+      console.log('You cant do this!');
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('You cannot delete this user!!')
+          .textContent('Sorry!')
+          .ariaLabel('Alert Dialog Demo')
+          .ok('Ok!')
+          .openFrom('#left')
+      )
+    } else {
+        StaffService.deleteUser(user.id)
+    }
   };
 
+
   vm.status = '  ';
-    vm.showConfirm = function(ev, user) {
-      console.log('user: ', user);
-        var confirm = $mdDialog.confirm()
-        .title('ARE YOU SURE YOU WANT TO DELETE THIS USER?')
-        .targetEvent(ev)
-        .ok('Yep!')
-        .cancel('Nevermind');
-        $mdDialog.show(confirm).then(function() {
-            // vm.status = 'Deleted!';
-            console.log('ev: ', ev);
-            vm.deleteUser(user.id);
-          }, function() {
-            vm.status = 'Okay lets leave it there';
-          });
-    };
+  vm.showConfirm = function (ev, user) {
+    console.log('user: ', user);
+    var confirm = $mdDialog.confirm()
+      .title('ARE YOU SURE YOU WANT TO DELETE THIS USER?')
+      .targetEvent(ev)
+      .ok('Yep!')
+      .cancel('Nevermind');
+    $mdDialog.show(confirm).then(function () {
+      console.log('ev: ', ev);
+      vm.deleteUser(user);
+    }, function () {
+      vm.status = 'Okay lets leave it there';
+    });
+  };
 
 }]);
