8f2bd992dce21ccf8d262d3c8b86d8ad77c11247
Brendt Bly <brendt.bly@outlook.com>
2017-10-10 13:38:55 -0500

    ng-repeat for category buttons done

--- server/public/scripts/controllers/student.data.display.controller.js
+++ server/public/scripts/controllers/student.data.display.controller.js
@@ -1,6 +1,8 @@
-myApp.controller('StudentDataDisplayController', ['StudentService', 'StudentDataService', 'UserService', '$mdDialog', '$mdSidenav', function (StudentService, StudentDataService, UserService, $mdDialog, $mdSidenav) {
+myApp.controller('StudentDataDisplayController', ['StudentDataDisplayService', 'StudentDataService', 'UserService', '$mdDialog', '$mdSidenav', function (StudentDataDisplayService, StudentDataService, UserService, $mdDialog, $mdSidenav) {
     console.log('StudentDataDisplayController Loaded');
 
     var self = this;
+    self.studentDataDisplayService = StudentDataDisplayService;
 
+    
 }]);
--- server/public/scripts/services/student.data.display.service.js
+++ server/public/scripts/services/student.data.display.service.js
@@ -2,5 +2,31 @@ myApp.service('StudentDataDisplayService', ['$http', '$location', '$mdDialog', '
     console.log('StudentDataDisplayService loaded');
     var self = this;
     self.studentDataService = StudentDataService;
+self.selectedCategory = {
+    selectedCategory: ''
+}
+    self.organisms = {
+        plants: [
+        { underscore: 'bur_oak', space: 'Bur oak' },
+    { underscore: 'quaking_aspen', space: 'Quaking aspen' },
+    { underscore: 'common_buckthorn', space: 'Common buckthron' },
+    { underscore: 'paper_birch', space: 'Paper birch' },
+    { underscore: 'milkweed', space: 'Milkweed' },
+    { underscore: 'red_oak', space: 'Red oak' }
+        ],
+    mammals: [
+    { underscore: 'ground_squirrel', space: 'Ground squirrel' }
+    ],
+    birds: [
+    { underscore: 'ruby_throated_hummingbird', space: 'Ruby-throated hummingbird' },
+    { underscore: 'dark_eyed_junco', space: 'Dark-eyed junco' },
+    { underscore: 'eastern_bluebird', space: 'Eastern bluebird' }
+    ]
+}
 
+self.selectCategory = function (category) {
+    self.selectedCategory.selectedCategory = category;
+    console.log('selectCategory clicked');
+    
+}
 }]);
